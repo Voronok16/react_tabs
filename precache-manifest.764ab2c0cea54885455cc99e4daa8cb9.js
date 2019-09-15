511d81576d92abe1ba1c142b525a3faffc93d291
Dmytro <voronok16@gmail.com>
2019-09-15 19:48:46 +0300

    Updates

---
+++ precache-manifest.764ab2c0cea54885455cc99e4daa8cb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b49371bcc0d6798b9f4669b970a0d33",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "28118d2af7d91d85a2b8",
+    "url": "/react_tabs/static/css/main.320bc723.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/react_tabs/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "28118d2af7d91d85a2b8",
+    "url": "/react_tabs/static/js/main.9d698dcb.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
